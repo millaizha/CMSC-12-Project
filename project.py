cfb45f5b82958bb5dba62c15b0ea87475b307906
Mill <39644596+millaizha@users.noreply.github.com>
2022-10-29 08:33:35 +0800

    Catch Add Movie Conflict and Missing Inputs

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
+from audioop import add
 import PySimpleGUI as sg
-from datetime import datetime
+from datetimerange import DateTimeRange
 import ast
 
 sg.theme('DarkGrey5')
@@ -90,71 +91,81 @@ def cashierMain():
 
 # Admin
 def addMovie():
-    with open("movies.txt", "r") as f:
-        intMovieID = int(f.readline()) + 1
-
-    movieID = str(intMovieID).zfill(4)
-
     addMovieLayout = [
                     [sg.T("Enter Movie name: "), sg.Input(key = "-NAME-", do_not_clear = True, size = (20,1))],
                     [sg.T("Enter Movie Genre: "), sg.Input(key = "-GENRE-", do_not_clear = True, size = (20,1))],
                     [sg.T("Choose Parental Rating: "), sg.Radio("G", "RESTRICT", key = "-G-", default = True), sg.Radio("PG", "RESTRICT", key = "-PG-"), sg.Radio("SPG", "RESTRICT", key = "-SPG-"), sg.Radio("R18", "RESTRICT", key = "-R18-")],
                     [sg.T("Choose Cinema Venue: "), sg.Radio("1", "CINEMA", key = "-1-", default = True), sg.Radio("2", "CINEMA", key = "-2-"), sg.Radio("3", "CINEMA", key = "-3-")],
                     [sg.T("Choose Date: "), sg.Input(key='-DATE-', size=(20,1)), sg.CalendarButton("Open Calendar", close_when_date_chosen=True,  target='-DATE-', format = "%m-%d-%y", location=(0,0), no_titlebar=False)],
-                    [sg.T("Enter Start Time: "), sg.Input(key='-STARTHOUR-', size=(5,1)), sg.T(":"), sg.Input(key='-STARTMIN-', size=(5,1)), sg.Radio("AM", "START12H", key = "-STARTAM-"), sg.Radio("PM", "START12H", key = "-STARTPM-")],
-                    [sg.T("Enter End Time: "), sg.Input(key='-ENDHOUR-', size=(5,1)), sg.T(":"), sg.Input(key='-ENDMIN-', size=(5,1)), sg.Radio("AM", "END12H", key = "-ENDAM-"), sg.Radio("PM", "END12H", key = "-ENDPM-")],
+                    [sg.T("Enter Start Time: "), sg.Input(key='-STARTHOUR-', size=(5,1)), sg.T(":"), sg.Input(key='-STARTMIN-', size=(5,1)), sg.Radio("AM", "START12H", key = "-STARTAM-", default = True), sg.Radio("PM", "START12H", key = "-STARTPM-")],
+                    [sg.T("Enter End Time: "), sg.Input(key='-ENDHOUR-', size=(5,1)), sg.T(":"), sg.Input(key='-ENDMIN-', size=(5,1)), sg.Radio("AM", "END12H", key = "-ENDAM-", default = True), sg.Radio("PM", "END12H", key = "-ENDPM-")],
                     [sg.T("Enter Movie Price: "), sg.Input(key = "-PRICE-", do_not_clear = True, size = (20,1))],
                     [sg.Button("Add Movie"), sg.Button("Cancel")]
                     ]
 
     addMovie = sg.Window("Add Movie", addMovieLayout, modal = True)
 
+    input_key_list = [key for key, value in addMovie.key_dict.items()
+                    if isinstance(value, sg.Input)]
+
+
     while True:
         event, values = addMovie.read()
+
+        if values["-G-"]:
+            restrict = "G"
+        elif values["-PG-"]:
+            restrict = "PG"
+        elif values["-SPG-"]:
+            restrict = "SPG"
+        else:
+            restrict = "R18"
+
+        if values["-1-"]:
+            cinema = "1"
+        elif values["-PG-"]:
+            cinema = "2"
+        else:
+            cinema = "3"
+        
+        startTime = values["-STARTHOUR-"] + ":" + values["-STARTMIN-"]
+        if values["-STARTAM-"]:
+            startTime += "AM"
+        else:
+            startTime += "PM"
+
+        endTime = values["-ENDHOUR-"] + ":" + values["-ENDMIN-"]
+        if values["-ENDAM-"]:
+            endTime += "AM"
+        else:
+            endTime += "PM"
+
+        inConflict = False
 
         if event == sg.WIN_CLOSED:
             break
         else:
-            addMovie.close()
             if event == "Add Movie":
-                addMovieInfo(movieID, intMovieID, values)
-            elif event == "Cancel":
-                adminMain()
+                for k, v in movies.items():
+                    if v[3] == cinema and v[4] == values["-DATE-"] and (startTime in DateTimeRange(v[5], v[6]) or endTime in DateTimeRange(v[5], v[6])):
+                        sg.Popup(f"This movie will be in conflict with {k} - {v[0]} [Cinema {v[3]}]: {v[4]} {v[5]} - {v[6]}.")
+                        inConflict = True
+                if all(map(str.strip, [values[key] for key in input_key_list])) and not inConflict:
+                    addMovie.close()
+                    addMovieInfo(restrict, cinema, startTime, endTime, values)
+                    adminMain()
+                elif not all(map(str.strip, [values[key] for key in input_key_list])):
+                    sg.popup("Some inputs are missed!")
 
-    addMovie.close()
+def addMovieInfo(restrict, cinema, startTime, endTime, values):
+    with open("movies.txt", "r") as f:
+        intMovieID = int(f.readline()) + 1
 
-def addMovieInfo(movieID, intMovieID, values):
-    if values["-G-"]:
-        restrict = "G"
-    elif values["-PG-"]:
-        restrict = "PG"
-    elif values["-SPG-"]:
-        restrict = "SPG"
-    else:
-        restrict = "R18"
-
-    if values["-1-"]:
-        cinema = "1"
-    elif values["-PG-"]:
-        cinema = "2"
-    else:
-        cinema = "3"
-    
-    startTime = values["-STARTHOUR-"] + ":" + values["-STARTMIN-"]
-    if values["-STARTAM-"]:
-        startTime += "AM"
-    else:
-        startTime += "PM"
-
-    endTime = values["-ENDHOUR-"] + ":" + values["-ENDMIN-"]
-    if values["-ENDAM-"]:
-        endTime += "AM"
-    else:
-        endTime += "PM"
+    movieID = str(intMovieID).zfill(4)
 
     movies[movieID] = [values["-NAME-"], values["-GENRE-"], restrict, cinema, values["-DATE-"], startTime, endTime, int(values["-PRICE-"])]
     booked[movieID] = []
-    print(movies[movieID])
+
     with open("movies.txt", "w") as f:
         f.write(str(intMovieID))
         print("\n", movies, file = f)
