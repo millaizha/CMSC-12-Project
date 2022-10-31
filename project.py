9c7b8644352c6a4b6b90400650046576f9735018
Mill <39644596+millaizha@users.noreply.github.com>
2022-10-31 12:03:22 +0800

    Add View Movies GUI Feature

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+from datetime import datetime
 from datetimerange import DateTimeRange
 import ast
 
@@ -50,7 +51,7 @@ def adminMain():
             elif event == "Delete movie":
                 deleteMovie()
             elif event == "View movie":
-                pass
+                viewMovieAdmin()
             elif event == "Go to Users":
                 chooseUser()
             elif event == "Exit":
@@ -351,11 +352,11 @@ def deleteMovieByID():
     movieInfo = {}
 
     showMovieList = [
-                            [sg.T("Select a Movie:")],
-                            [sg.T("Search: "), sg.Input(size=(20, 1), enable_events=True, key='-SEARCH-')],
-                            [sg.Listbox(movieList, size=(40, 10), enable_events = True, key='-MOVIE-')],
-                            [sg.Button("Cancel")]
-                            ]
+                    [sg.T("Select a Movie:")],
+                    [sg.T("Search: "), sg.Input(size=(20, 1), enable_events=True, key='-SEARCH-')],
+                    [sg.Listbox(movieList, size=(40, 10), enable_events = True, key='-MOVIE-')],
+                    [sg.Button("Cancel")]
+                    ]
 
     showMovieInfo = [
                     [sg.T("Movie Information:")],
@@ -513,6 +514,210 @@ def deletePopup(data):
         else:
             window.close()
             return "Yes"
+
+def viewMovieAdmin():
+    viewMovieAdminLayout = [
+                           [sg.Button("View all movies")],
+                           [sg.Button("View all movies in a cinema by day")],
+                           [sg.Button("View details of a movie")],
+                           [sg.Button("View all movie screening by name")],
+                           [sg.Button("Go back")]
+                           ]
+
+    viewMovieAdmin = sg.Window("View Movie", viewMovieAdminLayout, element_justification = "c")
+
+    while True:
+        event, values = viewMovieAdmin.read()
+
+        if event == sg.WIN_CLOSED:
+            break
+
+        if event == "View all movies":
+            viewAllMoviesAdmin()
+        elif event == "View all movies in a cinema by day":
+            viewMovieInCinema()
+        elif event == "View details of a movie":
+            viewMovieDetails()
+        elif event == "View all movie screening by name":
+            viewMovieByName()
+        elif event == "Go back":
+            viewMovieAdmin.close()
+            adminMain()
+
+def viewAllMoviesAdmin():
+    movieList = [f"{k} - {v[0]}" for k, v in movies.items()]
+
+    showMovieList = [
+                    [sg.T("Movie List:")],
+                    [sg.Listbox(movieList, size=(40, 10), enable_events = True, key='-MOVIE-')],
+                    [sg.Button("Go back")]
+                    ]
+
+    viewAllMoviesAdmin = sg.Window("View Movies", showMovieList, element_justification = "c")
+
+    while True:
+        event, values = viewAllMoviesAdmin.read()
+
+        if event == sg.WIN_CLOSED:
+            break
+        if event == "Go back":
+            viewAllMoviesAdmin.close()
+
+def viewMovieInCinema():
+    movieList = []
+
+    searchMovie = [
+                  [sg.T("Select a date:"), sg.Input(key='-DATE-', size=(20,1)), sg.CalendarButton("Open Calendar", close_when_date_chosen=True,  target='-DATE-', format = "%m-%d-%y", no_titlebar=False)],
+                  [sg.T("Select Cinema:"), sg.Radio("1", "CINEMA", key = "-1-", default = True), sg.Radio("2", "CINEMA", key = "-2-"), sg.Radio("3", "CINEMA", key = "-3-")],
+                  [sg.Button("Search")],
+                  [sg.Button("Go back")]
+                  ]
+
+    showMovieList = [
+                    [sg.T("Movie Search Results")],
+                    [sg.Listbox(movieList, size=(50, 7), enable_events = True, key='-MOVIELIST-')],
+                    [sg.Button("Clear")]
+                    ]
+    
+    viewMovieInCinemaLayout = [
+                              [sg.Column(searchMovie, element_justification = "c"), sg.Column(showMovieList, element_justification = "c")]
+                              ]
+
+    viewMovieInCinema = sg.Window("View Movies", viewMovieInCinemaLayout)
+
+    while True:
+        event, values = viewMovieInCinema.read()
+
+        if values["-1-"]:
+            cinema = "1"
+        elif values["-2-"]:
+            cinema = "2"
+        elif values["-3-"]:
+            cinema = "3"
+
+        date = values["-DATE-"]
+
+        if event == sg.WIN_CLOSED:
+            break
+
+        if event == "Search":
+            viewMovieInCinema["-MOVIELIST-"].update([])
+            movieList = []
+
+            for k, v in movies.items():
+                if v[3] == cinema and v[4] == date:
+                    movieList.append(f"{k} - {v[0]}")
+
+            if len(movieList) == 0:
+                sg.popup(f"There are no movies on {date} at Cinema {cinema}")
+            else:
+                viewMovieInCinema["-MOVIELIST-"].update(movieList)
+        elif event == "Clear":
+            viewMovieInCinema["-MOVIELIST-"].update([])
+            movieList = []
+        elif event == "Go back":
+            viewMovieInCinema.close()
+            break
+
+def viewMovieDetails():
+    movieList = [f"{k} - {v[0]}" for k, v in movies.items()]
+    movieInfo = {}
+
+    showMovieList = [
+                    [sg.T("Select a Movie:")],
+                    [sg.T("Search: "), sg.Input(size=(20, 1), enable_events=True, key='-SEARCH-')],
+                    [sg.Listbox(movieList, size=(40, 10), enable_events = True, key='-MOVIE-')],
+                    [sg.Button("Cancel")]
+                    ]
+
+    showMovieInfo = [
+                    [sg.T("Movie Information:")],
+                    [sg.Listbox(movieInfo, size=(50, 8), enable_events = True, key='-MOVIEINFO-')],
+                    [sg.Button("Clear")]
+                    ]
+    viewMovieDetailsLayout = [
+                      [sg.Column(showMovieList, element_justification = "c"), sg.Column(showMovieInfo, element_justification="c")]
+                      ]
+    viewMovieDetails = sg.Window("View Movie", viewMovieDetailsLayout)
+
+    while True:
+        event, values = viewMovieDetails.read()
+
+        if event == sg.WIN_CLOSED:
+            break
+
+        if values['-SEARCH-'] != '':
+            search = values['-SEARCH-']
+            new_values = [x for x in movieList if search in x]
+            viewMovieDetails['-MOVIE-'].update(new_values) 
+        else:
+            viewMovieDetails['-MOVIE-'].update(movieList)
+
+        if event == '-MOVIE-' and len(values['-MOVIE-']):
+            movieKey = str(values["-MOVIE-"])[2:6]
+            movieInfo = [f"Movie ID: {movieKey}", f"Movie Name: {movies[movieKey][0]}", 
+                        f"Movie Genre: {movies[movieKey][1]}", f"Parental Ratings: {movies[movieKey][2]}",
+                        f"Cinema Room: {movies[movieKey][3]}", f"Date and Time of Viewing: {movies[movieKey][4]} {movies[movieKey][5]} - {movies[movieKey][6]}",
+                        f"Price: P{movies[movieKey][7]}", f"Total Earnings from Booked Seats: P{movies[movieKey][7] * len(booked[movieKey])}"]
+            viewMovieDetails["-MOVIEINFO-"].update(movieInfo)
+
+        if event == "Clear":
+            viewMovieDetails['-MOVIEINFO-'].update([])
+        elif event == "Cancel":
+            viewMovieDetails.close()
+
+def viewMovieByName():
+    viewDict = {}
+    movieList = []
+
+    searchMovie = [
+                  [sg.T("Search a Movie:"), sg.Input(key='-NAME-', size=(20,1))],
+                  [sg.Button("Search")],
+                  [sg.Button("Go back")]
+                  ]
+
+    showMovieList = [
+                    [sg.T("Movie Search Results")],
+                    [sg.Listbox(movieList, size=(50, 7), enable_events = True, key='-MOVIELIST-')],
+                    [sg.Button("Clear")]
+                    ]
+    
+    viewMovieByNameLayout = [
+                              [sg.Column(searchMovie, element_justification = "c"), sg.Column(showMovieList, element_justification = "c")]
+                              ]
+
+    viewMovieByName = sg.Window("View Movies", viewMovieByNameLayout)
+
+    while True:
+        event, values = viewMovieByName.read()
+
+        name = values["-NAME-"]
+
+        if event == sg.WIN_CLOSED:
+            break
+
+        if event == "Search":
+            for k, v in movies.items():
+                if name == v[0]:
+                    viewDict[k] = v
+
+            viewDict = sorted(viewDict, key = lambda k: ([datetime.strptime((viewDict[k][4]), '%m-%d-%y').date()], [datetime.strptime((viewDict[k][5]), '%I:%M%p').time()]))
+            movieList = [f"{k} - {movies[k][0]} [Cinema {movies[k][3]}] {movies[k][4]} {movies[k][5]} - {movies[k][6]}" for k in viewDict]
+
+            if len(viewDict) == 0:
+                sg.popup(f"There is no movie named {name}")
+            else:
+                viewMovieByName["-MOVIELIST-"].update(movieList)
+
+            viewDict = {}
+            movieList = []
+        
+        elif event == "Clear":
+            viewMovieByName["-MOVIELIST-"].update([])
+
+        elif event == "Go back":
+            viewMovieByName.close()
+            break
 
 def updateFiles():
     with open("movies.txt", "r") as f:
